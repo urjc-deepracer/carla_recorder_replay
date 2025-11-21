055ff84afce70ea9202c992d46f6cd8cbd190e0b
Roberto Calvo <roberto.calvo@urjc.es>
2025-11-21 09:32:59 +0100

    refactor recorder

--- recorder.py
+++ recorder.py
@@ -17,69 +17,82 @@
 #  along with this program.  If not, see http://www.gnu.org/licenses/. 
 #
 #  Author : Roberto Calvo Palomino <roberto.calvo at urjc dot es
-#
+#           Sergio Robledo <s.robledo.2021 at alumnos dot urjc dot es>
 
  
  
-import carla
-import random
-import pygame
-import numpy as np
+
+import os
 import sys
 import csv
+import time
+import argparse
+import numpy as np
 
-log_filename = "/tmp/town04_autopilot.log"
-CSVNAME = "speedTown4.csv"
+import carla
+import pygame
+import random
+
+
+RATE_CONTROL_LOOP = 30
+VEHICLE_MODEL = "model3"
+CSV_FILENAME = "data.csv"
 
 last_time = None
 count = 0
 
-def game_loop():
+def game_loop(args):
 
     pygame.init()
-    pygame.display.set_caption("CARLA Town04 autopilot")
+    pygame.display.set_caption("CARLA recorder")
     display_width, display_height = 800, 600
     screen = pygame.display.set_mode((display_width, display_height))
 
-    client = carla.Client('localhost', 3010)
+    client = carla.Client('localhost', args.port)
     client.set_timeout(10.0)
 
-    world = client.load_world('Town04')
+    world = client.load_world(args.town)
 
-    
-    # Speed CSV settings
-    csv_path = CSVNAME
-    csv_fh = open(csv_path, "w", newline="")
+    log_path = args.log_path + "/" + str(int(time.time())) + "_" + args.town + "/"
+    os.makedirs(log_path, exist_ok=True)
+
+    # Speed CSV settings    
+    csv_file_path = log_path + CSV_FILENAME
+    csv_fh = open(csv_file_path, "w", newline="")
     csv_writer = csv.writer(csv_fh)
     csv_writer.writerow(["sim_time", "speed_m_s"])
 
     blueprint_library = world.get_blueprint_library()
 
     # Select vehicle
-    vehicle_bp = blueprint_library.filter('model3')[0]
+    vehicle_bp = blueprint_library.filter(VEHICLE_MODEL)[0]
+    print(vehicle_bp)
     spawn_points = world.get_map().get_spawn_points()
     spawn_point = random.choice(spawn_points)    
     vehicle = world.spawn_actor(vehicle_bp, spawn_point)
 
     # Traffic manager
-    tm = client.get_trafficmanager(3020)
+    tm = client.get_trafficmanager(args.tport)
     tm_port = tm.get_port()        
-    
 
-    # Spain the bicycle in front of the car
-    moto_bp = blueprint_library.filter('vehicle.diamondback.century')[0] 
-    moto_transform = carla.Transform(
-        spawn_point.location + spawn_point.get_forward_vector() * 10.0,  # 10 meters in front of
-        spawn_point.rotation
-    )
-    motorcycle = world.try_spawn_actor(moto_bp, moto_transform)
+    if (args.extra_actor):
+        # Spawn the bicycle in front of the car
+        moto_bp = blueprint_library.filter('vehicle.diamondback.century')[0] 
+        moto_transform = carla.Transform(
+            spawn_point.location + spawn_point.get_forward_vector() * 10.0,  # 10 meters in front of
+            spawn_point.rotation
+        )
+        motorcycle = world.try_spawn_actor(moto_bp, moto_transform)
+        motorcycle.set_autopilot(True, tm_port)
 
     # Enable autopilot
-    motorcycle.set_autopilot(True, tm_port)
     vehicle.set_autopilot(True, tm_port)
 
     # Start the log and recording
     # Important! Make sure you start the recording after spawn all your actors
+    
+    log_filename = log_path + args.town + ".log"
+    print(log_filename)
     client.start_recorder(log_filename, True)
 
     # Camera RGB
@@ -121,9 +134,11 @@ def game_loop():
         while True:
             
             rel_time = world.get_snapshot().timestamp.elapsed_seconds - t0
+            # Get vehicle speed
+            raw_vel = vehicle.get_velocity()
 
             # rate of this control loop
-            clock.tick(30)
+            clock.tick(RATE_CONTROL_LOOP)
             
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
@@ -134,8 +149,7 @@ def game_loop():
 
             pygame.display.flip()
 
-            # Get vehicle speed
-            raw_vel = vehicle.get_velocity()
+            
             speed = float(np.linalg.norm([raw_vel.x, raw_vel.y, raw_vel.z]))
             csv_writer.writerow([f"{rel_time:.6f}", f"{speed:.6f}"])
 
@@ -153,7 +167,7 @@ def game_loop():
         if vehicle is not None:
             vehicle.destroy()
 
-        if motorcycle is not None:
+        if args.extra_actor and motorcycle is not None:
             motorcycle.destroy()
 
         csv_fh.flush()
@@ -164,7 +178,27 @@ def game_loop():
 
 
 if __name__ == '__main__':
+
+    parser = argparse.ArgumentParser(description="recorder")
+
+    parser.add_argument("--log_path", type=str, default=os.getcwd() + "/logs/",
+                        help="Folder to save logs")
+    
+    parser.add_argument("--town", "--carla-town", type=str, default="Town04",
+                        help="Map where run recorder")
+    
+    parser.add_argument("--port", "--carla-port", type=int, default=3010,
+                        help="Default port for CARLA")
+    
+    parser.add_argument("--tport","--carla-traffic-port", type=int, default=3020,
+                        help="Default port for CARLA")
+    
+    parser.add_argument("--extra_actor", "--carla-extra-actor", action="store_true",
+                        help="Enable an extra actor in fron of the ego vehicle")
+
+    args = parser.parse_args()
+
     try:
-        game_loop()
+        game_loop(args)
     except SystemExit:
         pass
